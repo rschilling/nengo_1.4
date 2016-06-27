97bb843cfa222dae2bacc2607e53c496e02941dd
Richard Schilling <coderroadie@gmail.com>
2016-06-26 23:57:01 -0700

    Add sample network to service for testing. Creating the network takes a long time. But, finally, we're able to start developing networks and understanding more about integrators.  So, that's good.

--- simulator-android/app/src/main/java/cognition/com/nengo/NengoActivity.java
+++ simulator-android/app/src/main/java/cognition/com/nengo/NengoActivity.java
@@ -1,6 +1,7 @@
 package cognition.com.nengo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class NengoActivity extends Activity {
@@ -9,5 +10,12 @@ public class NengoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nengo);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        startService(new Intent(this, NengoService.class));
     }
 }
